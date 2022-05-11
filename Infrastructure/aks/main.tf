f575a77a8fa2c97a70ea08c01c47a0ea99839a58
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-11 14:34:44 +0530

    Initial Version

---
+++ Infrastructure/aks/main.tf
@@ -0,0 +1,16 @@
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.2.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+
+provider "azurerm" {
+  features {}
+}
